a095b26ef9807ec7392581084e9343cb4c474466
Jian Liang <jian.liang@uky.edu>
2021-04-17 11:43:58 +0800

    try on SNSC

---
+++ tests/run_snsc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=gpu8
+#SBATCH --job-name=test_heat
+#SBATCH --output=test_heat.log
+#SBATCH --error=test_heat.log
+#SBATCH --nodes=1
+#SBATCH -n 4
+# #SBATCH --exclusive
+#SBATCH --cpus-per-task=4
+# #SBATCH --nodelist= 
+#SBATCH --time=0:30:00
+#SBATCH --gres=gpu:4
+
+# GPU8  36 Phyisical cores
+# DGX2  48 Phyisical cores
+export export OMP_NUM_THREADS=4
+
+mpirun -np 4 python test1.py > test1_snsc.out 2>&1
